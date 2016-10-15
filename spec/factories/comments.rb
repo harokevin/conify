814034569a840c9952408d392c4bd63e5e014d96
harokevin <harokevin0@gmail.com>
2016-10-15 12:23:26 -0700

    npm installed react-sidebar

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -5,12 +5,14 @@
 #  id         :integer          not null, primary key
 #  comment    :text
 #  event_id   :integer          not null
+#  user_id    :integer          not null
 #  rating     :integer
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 # Foreign Keys
 #
+#  fk_rails_03de2dc08c  (user_id => users.id)
 #  fk_rails_c6b77e33aa  (event_id => events.id)
 #
 
--- spec/factories/event_speakers.rb
+++ spec/factories/event_speakers.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: event_speakers
+#
+#  event_id   :integer
+#  speaker_id :integer
+#
+# Indexes
+#
+#  index_event_speakers_on_event_id    (event_id)
+#  index_event_speakers_on_speaker_id  (speaker_id)
+#
+
 
 FactoryGirl.define do
   factory :event_speaker do
